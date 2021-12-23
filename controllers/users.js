c3696d76adec655f16a96b9496183798bb2a2b2b
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-12-24 00:02:24 +0300

    fixed

--- controllers/users.js
+++ controllers/users.js
@@ -89,7 +89,7 @@ module.exports.login = (req, res, next) => {
         { expiresIn: '7d' },
 
       );
-      return res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, /* sameSite: 'none', secure: true */ sameSite: true}).send({ token });
+      return res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: 'none', secure: true}).send({ token });
     })
     .catch(() => {
       next(new AuthError('Неправильные почта или пароль'));
